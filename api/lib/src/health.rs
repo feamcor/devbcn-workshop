403869cfce18f6c46af2aa4db4be6660e6b67e0d
Fábio Corrêa <feamcor@gmail.com>
2023-08-10 11:44:54 -0300

    Added unit and integration tests.

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,11 +1,28 @@
 use actix_web::{HttpResponse, web::{self, ServiceConfig}};
 
+pub const API_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub fn service(configuration: &mut ServiceConfig) {
     configuration.route("/health", web::get().to(health));
 }
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
-        .append_header(("version", env!("CARGO_PKG_VERSION")))
+        .append_header(("version", API_VERSION))
         .finish()
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::http::StatusCode;
+    use super::*;
+
+    #[actix_rt::test]
+    async fn health_check_works() {
+        let response = health().await;
+        assert!(response.status().is_success());
+        assert_eq!(response.status(), StatusCode::OK);
+        let version = response.headers().get("version").and_then(|v| v.to_str().ok());
+        assert_eq!(version, Some(API_VERSION));
+    }
 }
