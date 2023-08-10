a1dd85bf97cc60e071eced1519a4252cdb9618a9
Fábio Corrêa <feamcor@gmail.com>
2023-08-10 10:20:49 -0300

    Moved endpoints to library.

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,37 +1,21 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 use sqlx::Pool;
-
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
-
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting database version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-    tracing::info!("Handling database version result");
-    match result {
-        Ok(version) => version,
-        Err(e) => format!("Error: {:?}", e),
-    }
-}
-
+use api_lib::health::*;
 
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: Pool<sqlx::Postgres>,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    // initialize the database if not already initialized
+    tracing::info!("Initializing the database if not already done");
     pool.execute(include_str!("../../db/schema.sql"))
         .await
         .map_err(CustomError::new)?;
+    tracing::info!("Wrapping database pool on application data");
     let pool = actix_web::web::Data::new(pool);
+    tracing::info!("Registering application configuration");
     let config = move |conf: &mut ServiceConfig| {
         conf.app_data(pool)
             .service(hello_world)
