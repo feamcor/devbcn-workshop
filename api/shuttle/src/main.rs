b51fa774e787f2a606fb7468e31fa55376319b7f
Fábio Corrêa <feamcor@gmail.com>
2023-08-10 09:57:38 -0300

    Added instrumentation.

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -11,9 +11,11 @@ async fn hello_world() -> &'static str {
 
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting database version");
     let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
         .fetch_one(db.get_ref())
         .await;
+    tracing::info!("Handling database version result");
     match result {
         Ok(version) => version,
         Err(e) => format!("Error: {:?}", e),
