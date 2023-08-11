bc9f35c215dad2e78fdf26aed1029e8c550d743c
Fábio Corrêa <feamcor@gmail.com>
2023-08-11 18:21:09 -0300

    Added frontend.

---
+++ front/src/components/mod.rs
@@ -0,0 +1,11 @@
+mod button;
+mod header;
+mod film_card;
+mod film_modal;
+mod footer;
+
+pub use button::Button;
+pub use header::Header;
+pub use film_card::FilmCard;
+pub use film_modal::FilmModal;
+pub use footer::Footer;
